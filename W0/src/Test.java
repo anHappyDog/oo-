742f8d5323a87c1c445af3c3fa8da67392e37f74
anHappyDog <87591921+anHappyDog@users.noreply.github.com>
2022-11-01 16:27:23 +0800

    Add files via upload
    GIT 的安装和使用

---
+++ W0/src/Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args)
+    {
+        long a;
+        long b;
+        Scanner input = new Scanner(System.in);
+        a = input.nextLong();
+        b = input.nextLong();
+        System.out.println(a + b);
+
+    }
+}
